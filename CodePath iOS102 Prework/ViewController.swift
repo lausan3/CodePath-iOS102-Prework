28f47713ccdbf48217da9b8c21e7936601927a8f
lausan3 <92795688+lausan3@users.noreply.github.com>
2022-12-26 00:51:50 -0500

    Initial app push
    Optional features to be added and completed README

---
+++ CodePath iOS102 Prework/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  CodePath iOS102 Prework
+//
+//  Created by Anthony on 12/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var schoolTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var academicYearSegmentControl: UISegmentedControl!
+    @IBOutlet weak var numberOfPets: UILabel!
+    @IBOutlet weak var petsStepper: UIStepper!
+    @IBOutlet weak var wantMorePetsSwitch: UISwitch!
+    
+    @IBAction func petsStepperEvent(_ sender: UIStepper) {
+        
+        numberOfPets.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func introduceSelfButton(_ sender: UIButton) {
+        
+        // assigns the text on the current selected segment to year
+        let year = academicYearSegmentControl.titleForSegment(at: academicYearSegmentControl.selectedSegmentIndex)
+        
+        // generates the introduction text based on inputs
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolTextField.text!). I am currently in my \(year!) year and I own \(numberOfPets.text!) pets. It is \(wantMorePetsSwitch.isOn) that I want more pets."
+        
+        // creates the alert.
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        // allows alert box dismissal
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        
+        present(alertController, animated:true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodePath iOS102 PreworkUITests/CodePath_iOS102_PreworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CodePath_iOS102_PreworkUITestsLaunchTests.swift
+//  CodePath iOS102 PreworkUITests
+//
+//  Created by Anthony on 12/25/22.
+//
+
+import XCTest
+
+final class CodePath_iOS102_PreworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
